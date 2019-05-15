6dc92bc6b37d12b0f701c20669d359ecd0a0b45d
wangkun <kennywhite@163.com>
2019-05-15 22:14:48 +0800

    update

---
+++ src/main/java/com/disruptor/LongEventHandler.java
@@ -0,0 +1,12 @@
+package com.disruptor;
+
+import com.lmax.disruptor.EventHandler;
+
+public class LongEventHandler implements EventHandler<LongEvent>
+{
+    @Override
+    public void onEvent(LongEvent event, long sequence, boolean endOfBatch)
+    {
+        System.out.println("Event: " + event);
+    }
+}
